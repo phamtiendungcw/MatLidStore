ca34071951d56397331c94cad523c66755d120cd
Pham Tien Dung <dungkbkbhn@gmail.com>
2024-07-14 16:04:26 +0700

    refactor: Update component selector and page title in app.component.ts and index.html

--- src/MLS.WebUI/src/app/modules/app.component.ts
+++ src/MLS.WebUI/src/app/modules/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'MatLid Store';
-}
+export class AppComponent {}
