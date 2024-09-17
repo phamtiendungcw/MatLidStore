fa291574950ee751990afc33ce8fe0a3e667d0b2
Pham Tien Dung <dungkbkbhn@gmail.com>
2024-09-18 04:01:44 +0700

    Refactor theme module to include CommonModule and RouterLink

--- src/MLS.MatLidStoreUI/src/app/theme/components/header/header.component.ts
+++ src/MLS.MatLidStoreUI/src/app/theme/components/header/header.component.ts
@@ -1,14 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/core/services/account.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  public logoUrl = '';
+export class HeaderComponent {
+  public logoUrl = '/assets/Images/MatLidStore_Logo.png';
+  public isLoggedIn = this.accountServices.isLoggedIn();
+  isMenuOpened = false;
+  isMenuProductOpened = false;
 
-  ngOnInit(): void {
-    this.logoUrl = '/assets/Images/MatLidStore_Logo.png';
+  constructor(
+    private accountServices: AccountService,
+    private router: Router
+  ) {}
+
+  toggleMenu() {
+    this.isMenuOpened = !this.isMenuOpened;
+  }
+
+  toggleProductMenu() {
+    this.isMenuProductOpened = !this.isMenuProductOpened;
+  }
+
+  logoutClick() {
+    this.router.navigate(['/login']);
+    this.accountServices.setLoggedIn(false);
+  }
+
+  loginClick() {
+    this.router.navigate(['/login']);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    const target = event.target as HTMLElement;
+
+    // Kiểm tra nếu click nằm ngoài phần tử menu và các nút toggle menu
+    const isClickOutsideMenu =
+      !target.closest('.menu-container') &&
+      !target.closest('.toggle-menu-button') &&
+      !target.closest('.toggle-product-menu-button'); // Thêm class của các nút mở menu
+
+    if (isClickOutsideMenu) {
+      this.isMenuOpened = false;
+      this.isMenuProductOpened = false;
+    }
   }
 }
--- src/MLS.MatLidStoreUI/src/app/theme/theme.module.ts
+++ src/MLS.MatLidStoreUI/src/app/theme/theme.module.ts
@@ -4,10 +4,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, PaginationComponent, SearchInputComponent],
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   exports: [HeaderComponent, FooterComponent],
 })
 export class ThemeModule {}
