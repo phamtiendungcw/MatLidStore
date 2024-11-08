d33e5b56565a93e7df9ffe299932ee3329597e83
Pham Tien Dung <dungkbkbhn@gmail.com>
2024-11-09 03:13:22 +0700

    Update project metadata and add new CSS files
    Updated project metadata comments across multiple files to include contact details, last modified dates, and updated copyright information. Reformatted license information for better readability. Added new CSS and source map files for various components, including charset declarations. Updated HTML and SCSS files to include new project metadata.

--- src/MLS.MatLidStoreUI/src/app/theme/pages/account/login-page/login-page.component.ts
+++ src/MLS.MatLidStoreUI/src/app/theme/pages/account/login-page/login-page.component.ts
@@ -1,15 +1,16 @@
 /*
- * Project: MatLidStore(https://github.com/phamtiendungcw/matlidstore)
+ * Project: MatLidStore (https://github.com/phamtiendungcw/matlidstore)
  * File: login-page.component.ts
- * Author: phamtiendungcw(https://github.com/phamtiendungcw)
- * Created on: 15:34, 07/11/2024
- *
- * Copyright (c) 2024 Pham Tien Dung. All rights reserved.
- *
+ * Author: phamtiendungcw (https://github.com/phamtiendungcw)
+ * Contact: Pham Tien Dung (phamtiendungcw@gmail.com)
+ * Last Modified: Thứ Bảy, 09-11-2024 | 02:57 SA
+ * --------
+ * Copyright © 2023 - 2024 Pham Tien Dung, MatLidStore Ltd. All rights reserved.
+ * --------
  * Description:
  *   This file is part of the MLS.MatLidStoreUI project.
  *   Unauthorized copying or distribution of this file, via any medium, is strictly prohibited.
- *
+ * --------
  * License: Proprietary and confidential.
  */
 
