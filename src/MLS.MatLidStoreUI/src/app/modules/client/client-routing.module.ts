e2b795eaac33fc6898038f10d0b9722196fb3d41
Pham Tien Dung <dungkbkbhn@gmail.com>
2024-11-08 15:54:48 +0700

    Add metadata comments and reformat various files
    - Added project metadata comments to multiple files for better documentation.
    - Updated `.eslintrc.json` and `angular.json` to format arrays and objects with multi-line syntax for improved readability.
    - Changed `request` type in `launch.json` from `launch` to `attach`.
    - Incremented version in `package-lock.json` and `package.json` from `0.0.0` to `0.0.1`.
    - Made `http` property readonly in `mls-data.service.ts`.
    - Replaced `accountService.isLoggedIn()` with `accountService.getCurrentUser()` in `auth.guard.ts`.
    - Adjusted margin in `admin-home.component.html` by removing `-m-24` class.
    - Added CSS classes for better styling in `footer.component.scss` and `header.component.scss`.
    - Added `Logout` button and additional styling to `admin-navbar.component.html`.
    - Reformatted `motion-tailwind.css` for consistent indentation and line breaks.
    - Improved formatting in `index.html`, `tsconfig.app.json`, `tsconfig.json`, and `tsconfig.spec.json`.
    - Modified SVG content in `github-mark-white.svg` and `github-mark.svg` for better readability.

--- src/MLS.MatLidStoreUI/src/app/modules/client/client-routing.module.ts
+++ src/MLS.MatLidStoreUI/src/app/modules/client/client-routing.module.ts
@@ -1,3 +1,18 @@
+/*
+ * Project: MatLidStore(https://github.com/phamtiendungcw/matlidstore)
+ * File: client-routing.module.ts
+ * Author: phamtiendungcw(https://github.com/phamtiendungcw)
+ * Created on: 15:34, 07/11/2024
+ *
+ * Copyright (c) 2024 Pham Tien Dung. All rights reserved.
+ *
+ * Description:
+ *   This file is part of the MLS.MatLidStoreUI project.
+ *   Unauthorized copying or distribution of this file, via any medium, is strictly prohibited.
+ *
+ * License: Proprietary and confidential.
+ */
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
--- src/MLS.MatLidStoreUI/tailwind.config.js
+++ src/MLS.MatLidStoreUI/tailwind.config.js
@@ -1,4 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+/*
+ * Project: MatLidStore(https://github.com/phamtiendungcw/matlidstore)
+ * File: tailwind.config.js
+ * Author: phamtiendungcw(https://github.com/phamtiendungcw)
+ * Created on: 15:31, 07/11/2024
+ *
+ * Copyright (c) 2024 Pham Tien Dung. All rights reserved.
+ *
+ * Description:
+ *   This file is part of the MLS.MatLidStoreUI project.
+ *   Unauthorized copying or distribution of this file, via any medium, is strictly prohibited.
+ *
+ * License: Proprietary and confidential.
+ */
+
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts,scss}"],
   theme: {
